22d81e10e7a52f486a26ba330fa9e8c20071b5c9
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-10-05 15:20:32 +0300

    testaus

--- unit-testing/src/example.ts
+++ unit-testing/src/example.ts
@@ -7,4 +7,15 @@ function subtract(a: number, b: number): number {
   return a - b;
 }
 
-export { add, subtract };
+// exercise 1
+function reverseString(input: string): string {
+  return input.split('').reverse().join('');
+}
+
+function isValidEmail(email: string): boolean {
+  // Regular expression for a basic email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+
+export { add, subtract, reverseString, isValidEmail };
--- unit-testing/test/example.test.ts
+++ unit-testing/test/example.test.ts
@@ -1,6 +1,6 @@
 // example.test.ts
 import { it, expect, describe } from 'vitest';
-import { add, subtract } from '../src/example';
+import { add, isValidEmail, reverseString, subtract } from '../src/example';
 
 describe('Math operations', () => {
   it('should correctly add two numbers', () => {
@@ -9,5 +9,21 @@ describe('Math operations', () => {
 
   it('should correctly subtract two numbers', () => {
     expect(subtract(3, 2)).toBe(1);
+  });
+});
+
+describe('Exercises', () => {
+  it('should correctly reverse a string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+    expect(reverseString('')).toBe(''); // Test with an empty string
+    expect(reverseString('12345')).toBe('54321'); // Test with numbers
+    expect(reverseString('A man, a plan, a canal, Panama')).toBe(
+      'amanaP ,lanac a ,nalp a ,nam A',
+    ); // Test with a palindrome sentence
+  });
+
+  it('should validate email address (true/false)', () => {
+    expect(isValidEmail('user@example.com')).toBe(true);
+    expect(isValidEmail(1233)).toBe(false);
   });
 });
